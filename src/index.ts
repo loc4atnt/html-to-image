aa9b33d144e41e1c1221bb79805032a662cb1e20
Huỳnh Đức Nhâm <huynhducnham@gmail.com>
2024-11-13 18:59:20 +0700

    Test: Add debug clonedNode

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export async function toSvg<T extends HTMLElement>(
   await embedWebFonts(clonedNode, options)
   await embedImages(clonedNode, options)
   applyStyle(clonedNode, options)
+  console.log('clonedNode', clonedNode)
   const datauri = await nodeToDataURL(clonedNode, width, height)
   return datauri
 }
